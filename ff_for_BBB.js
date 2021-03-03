0795871495fad0dcfa149deaac86e9eb2796acd5
kkrell2016 <karim.krell@gmx.at>
2021-03-03 17:10:53 +0100

    Create ff_for_BBB.js

---
+++ ff_for_BBB.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         FastForward BBB AAU
+// @version      0.1
+// @description  Fast Foward Button
+// @author       Karim Krell
+// @match        https://bbb.aau.at/playback/presentation/2.0/playback.html*
+// @grant        none
+// ==/UserScript==
+
+window.setTimeout(()=>{
+$('<button class="button" style="background-image: url(https://pics.freeicons.io/uploads/icons/png/19603807591586787983-512.png);filter: invert(100%);" id="forward">Faster</button><span style="color:white" id="speedval">1x</span>').insertAfter("[class='acorn-timer']")
+
+console.log('losgehts')
+
+$('#forward').on("click",
+function(){
+let speed =parseFloat($('#video').attr('speed'))
+
+if(isNaN(speed)) {
+	speed=1.25
+} else {
+    speed+0.25>2?speed=1:speed+=0.25
+}
+	$('#video').attr('speed',speed)
+	$('#video')[0].playbackRate = speed
+	$('#speedval').text(speed+'x')
+}
+)
+}, 2000)
